73e5de21f04bd858a0c33eccd2db2393f4db1306
Jakub Balcerowicz <ku44@wp.pl>
2016-03-03 22:21:04 +0100

    zad6

---
+++ lab01/zad6.c
@@ -0,0 +1,10 @@
+// Sprawdzić i wyjaśnić działanie następujących fragmentów programów:
+// a. a = b = c = d = 0;
+// b. for (i=0; i<N; a[i++]=i);
+// c. for (i=0; i<N; a[++i]=i);
+// d. i=1; while ((i*=2)<N);
+
+a) a b c d maja przypisana wartosc 0
+b) i-ty indeks tablicy przyjmuje wartosc i oraz zwieksza sie o jeden
+c) i-ty indeks tablicy zwieksza sie o jeden i przyjmuje wartosc i
+d) i jest przemnazane przez 2 dopoki mniejsze od N
