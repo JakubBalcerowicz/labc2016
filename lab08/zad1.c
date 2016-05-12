d1a3b4442e1ff8230199d1068a53a0eaf2a1e8c0
JakubBalcerowicz <ku44@wp.pl>
2016-05-12 17:01:08 +0200

    Update zad1.c

--- lab08/zad1.c
+++ lab08/zad1.c
@@ -1,4 +1,9 @@
-#define INFO(kraj, waluta) W kraj OBOWIAZUJE waluta
- INFO(Polsce, zloty)
- INFO(Rosji, rubel)
- INFO(Slowacji, euro)
+// Jakie będą skutki rozwinięcia następującej makrodefinicji?
+
+#define INFO(kraj, waluta) W kraj OBOWIAZUJE waluta.
+INFO(Polsce, zloty)
+INFO(Rosji, rubel)
+INFO(Slowacji, euro)
+
+// Program wypisze kolejno:
+// W Polsce OBOWIAZUJE zloty. W Rosji OBOWIAZUJE rubel. W Slowacji OBOWIAZUJE euro.
