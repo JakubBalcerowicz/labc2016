ee01e76e9a6bc734c87b1331137dee8509ade488
JakubBalcerowicz <ku44@wp.pl>
2016-05-12 16:56:43 +0200

    Update zad1.c

--- lab07/zad1.c
+++ lab07/zad1.c
@@ -1,15 +1,28 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main (int count, char* arg[ ]) {
+void print_tab(int *ile, char *arg[], char *jakis)
+{
     int i;
-    for(i=count; i>1; i--){
-		if(i==count){
-			printf("%s", arg[i-1]);
+    for(i=*ile-1; i>0; i--)
+    {
+        if(i!=1)
+        {
+            printf("%s", arg[i]);
+            printf("%c", *jakis);
         }
         else
-			printf(" %s", arg[i-1]);
-	}
-printf("\n");
-return 0;
+        {
+            printf("%s\n", arg[i]);
+        }
+    }
+}
+
+int main (int ile_arg, char* arg[ ])
+{
+    printf("Podaj separator! :\n");
+    char c;
+    c = getchar();
+    print_tab(&ile_arg, arg,&c);
+    return 0;
 }
