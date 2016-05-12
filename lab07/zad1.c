65c4e17f9bf3cb77a4acc8973e110294fe5903ea
JakubBalcerowicz <ku44@wp.pl>
2016-05-12 17:07:50 +0200

    Update zad1.c

--- lab07/zad1.c
+++ lab07/zad1.c
@@ -1,30 +1,18 @@
-Napisać program   od_konca.c , którego wywołanie z argumentami spowoduje wypisanie tych argumentów w odwrotnej kolejności. Np. wywołanie
- ./od_konca  Ty. gdzie tam, zawsze tam, Byc
-wydrukuje tekst
- Byc tam, zawsze tam, gdzie Ty.
+// Napisać program od_konca.c , którego wywołanie z argumentami spowoduje wypisanie tych argumentów w odwrotnej kolejności.
+// Np. wywołanie:     ./od_konca  Ty. gdzie tam, zawsze tam, Byc
+// wydrukuje tekst:   Byc tam, zawsze tam, gdzie Ty.
 
 #include <stdio.h>
-#include <stdlib.h>
-
-void print_tab(int *ile, char *arg[], char *jakis)
+#include <string.h>
+int main(int argc, char* argv[])
 {
-    int i;
-    for(i=*ile-1; i>0; i--)
-    {
-        if(i!=1)
-        {
-            printf("%s", arg[i]);
-            printf("%c", *jakis);
-        }
-        else
-        {
-            printf("%s\n", arg[i]);
-        }
-    }
+  int i;
+  for(i=argc-1; i>0; i--)
+  {
+    printf("%s ", argv[i]);
+  }
+  return 0;
 }
-
-int main (int ile_arg, char* arg[ ])
-{
     printf("Podaj separator! :\n");
     char c;
     c = getchar();
