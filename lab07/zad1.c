b354f81b3c22ce2e98763daf0766b53968f94fdf
JakubBalcerowicz <ku44@wp.pl>
2016-05-12 16:57:41 +0200

    Update zad1.c

--- lab07/zad1.c
+++ lab07/zad1.c
@@ -1,3 +1,8 @@
+Napisać program   od_konca.c , którego wywołanie z argumentami spowoduje wypisanie tych argumentów w odwrotnej kolejności. Np. wywołanie
+ ./od_konca  Ty. gdzie tam, zawsze tam, Byc
+wydrukuje tekst
+ Byc tam, zawsze tam, gdzie Ty.
+
 #include <stdio.h>
 #include <stdlib.h>
 
